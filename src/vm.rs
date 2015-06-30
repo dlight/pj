b566b6ef68204f7d944739694f93eb302a830121
Elias Amaral <tolkiendili@gmail.com>
2015-06-30 10:29:17 -0300

    A somewhat radical redesign of the VM
    A bit of development log.
    
    I decided to abandon the idea of having the context not own the vector
    of bytecode instructions. My idea of sharing instructions between many
    contexts was a little misguided at this point, so that's ok.
    
    I also decided to abandon the idea of HashMap of functions and simply
    store the whole code in a single array.
    
    With that, I had to stop storing the "current function" as a slice
    (since I can't store both a Vec and a slice to it in the same data
    structure).
    
    I have src/ast.rs laying here uncomitted, but it doesn't really have
    anything important right now.
    
    Oh, and I gutted the main function. I'm doing cargo tests now.
    
    I asked those questions in public forums:
    
    https://stackoverflow.com/q/31067031
    https://stackoverflow.com/q/31103364
    https://stackoverflow.com/q/31123882
    https://www.reddit.com/r/rust/comments/3bkmuh

--- src/vm.rs
+++ src/vm.rs
@@ -1,15 +1,24 @@
-use std::collections::HashMap;
+#![allow(dead_code)]
 
 use self::Operation::*;
 use self::BranchCondition::*;
 use self::Opcode::*;
-use self::Error::*;
+use self::VmError::*;
+use self::LinkError::*;
 
 type Literal = i32;
 
+/// Position in the data stack
+/// Must be an unsigned integer
 type StackPos = usize;
-type CounterPos = usize;
-type JumpOffset = isize;
+
+/// Position in the vector of bytecode instructions
+/// Must be an unsigned integer
+type ProgramPos = usize;
+
+/// Jump offset in the vector of bytecode instructions
+/// Must be a signed integer
+type JumpOffset = i16;
 
 #[derive(Copy, Clone, Debug)]
 pub enum Operation {
@@ -24,83 +33,184 @@ pub enum BranchCondition {
 }
 
 #[derive(Copy, Clone, Debug)]
-pub enum Opcode<'a> {
+pub enum Opcode<T> {
     Push(Literal),
     Pop,
     Swap(StackPos),
     BinOp(Operation),
     Branch(BranchCondition, JumpOffset),
     Jump(JumpOffset),
-    Call(&'a str),
+    Call(T),
     Return,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum Error {
+pub enum VmError {
     StackUnderflow,
     TooFewParameters,
     InvalidStackPosition,
-    InvalidJumpOffset,
-    FunctionNotFound,
+    JumpOffsetTooLarge,
+    JumpOffsetTooSmall,
+    InvalidFunctionPosition,
+    ThereIsNoCode,
     Halted,
 }
 
-type Result<T> = ::std::result::Result<T, Error>;
-
-const MAIN_FUNCTION : &'static str = "main";
-
-#[derive(Copy, Clone, Debug)]
-struct ReturnValue<'a> {
-    function: &'a str,
-    counter: CounterPos,
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum LinkError {
+    FunctionNameNotFound
 }
 
-#[derive(Copy, Clone, Debug)]
-pub struct Function<'a> {
-    pub name: &'a str,
-    pub code: &'a [Opcode<'a>],
+type VmResult<T> = Result<T, VmError>;
+type LinkResult<T> = Result<T, LinkError>;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+struct Function {
+    start_pos: ProgramPos,
+    end_pos: ProgramPos
+}
+
+struct FunctionTable {
+    table: Vec<(Function, String)>
+}
+
+impl FunctionTable {
+    fn search_name(&self, name: &str) -> Option<(Function, usize)> {
+        let mut i = 0usize;
+        for &(f, ref s) in &self.table {
+            if name == s {
+                return Some((f, i));
+            }
+            i += 1;
+        }
+        None
+    }
+    fn search_start_pos(&self, start_pos: ProgramPos) -> Option<(Function, usize, &str)> {
+        let mut i = 0usize;
+        for &(f, ref s) in &self.table {
+            if start_pos == f.start_pos {
+                return Some((f, i, &s));
+            }
+            i += 1;
+        }
+        None
+    }
 }
 
 #[derive(Debug)]
-pub struct Context<'a> {
-    counter: CounterPos,
-    current_fun: Function<'a>,
-    program: HashMap<&'a str, Function<'a>>,
-
-    data_stack: Vec<Literal>,
-    call_stack: Vec<ReturnValue<'a>>,
+struct CallFrame {
+    return_pos: ProgramPos,
+    function_start: ProgramPos
 }
 
-impl<'a> Context<'a> {
-    pub fn new(program: HashMap<&'a str, Function<'a>>) -> Option<Context<'a>> {
-        program.get(MAIN_FUNCTION).map(|v| *v).map(|q| {
-            Context {
-                counter: 0,
-                current_fun: q,
-                program: program,
-                data_stack: vec![],
-                call_stack: vec![],
-            }
-        })
+
+pub struct Context {
+    counter: ProgramPos,
+
+    current_fun: Function,
+    current_fun_pos: usize,
+    functions: FunctionTable,
+
+    program: Vec<Opcode<ProgramPos>>,
+
+    data_stack: Vec<Literal>,
+    call_stack: Vec<CallFrame>,
+}
+
+impl Context {
+    pub fn new() -> Context {
+
+        Context {
+            counter: 0,
+
+            current_fun: Function { start_pos: 0, end_pos: 0 },
+            current_fun_pos: 0,
+            functions: FunctionTable { table: vec![] },
+
+            program: vec![],
+
+            data_stack: vec![],
+            call_stack: vec![],
+        }
     }
 
-    pub fn run(&mut self) -> (Vec<Literal>, Error) {
+    pub fn link(&mut self, new_functions: Vec<(&str, Vec<Opcode<&str>>)>) -> LinkResult<()> {
+        self.extend_function_table(&new_functions);
+
+        for (_, code) in new_functions {
+            try!(self.link_step(code));
+        }
+        Ok(())
+    }
+
+    fn extend_function_table(&mut self, new_functions: &[(&str, Vec<Opcode<&str>>)]) {
+        let mut start_pos = self.program.len() as ProgramPos;
+
+        for &(name, ref code) in new_functions {
+            let end_pos = start_pos + (code.len() as ProgramPos) - 1;
+
+            self.functions.table.push((Function {
+                start_pos: start_pos,
+                end_pos: end_pos
+            }, name.to_owned()));
+
+            start_pos = end_pos + 1;
+        }
+    }
+
+    fn link_step(&mut self, code: Vec<Opcode<&str>>) -> LinkResult<()> {
+        for v in code {
+            let instr : Opcode<ProgramPos> = match v {
+                Call(name) => {
+                    let (function, _) = try!(self.functions
+                                        .search_name(name)
+                                        .ok_or(FunctionNameNotFound));
+
+                    Call(function.start_pos)
+                },
+
+                // do not repeat yourself! this is intolerable..
+                
+                Push(lit) => Push(lit),
+                Pop => Pop,
+                Swap(pos) => Swap(pos),
+                BinOp(op) => BinOp(op),
+                Branch(cond, offset) => Branch(cond, offset),
+                Jump(offset) => Jump(offset),
+                Return => Return,
+            };
+
+            self.program.push(instr);
+        }
+        Ok(())
+    }
+
+    fn fun_name(&self) -> &str {
+        &self.functions.table[self.current_fun_pos].1
+    }
+
+    pub fn run(&mut self) -> (Vec<Literal>, VmError) {
         let mut result = Ok(());
 
+        if self.program.len() == 0 {
+            println!("There is no code");
+            return (vec![], ThereIsNoCode);
+        }
+
+        self.current_fun = self.functions.table[self.current_fun_pos].0;
+
         println!("Program: {:?}", self.program);
-        println!("Starting at {:?}", MAIN_FUNCTION);
+        println!("Starting at {:?}", self.fun_name());
 
         while let Ok(()) = result {
-            let last = self.current_fun.code.len() - 1;
-
-            result = if self.counter > last {
-                println!("Implicit return at end of {}", self.current_fun.name);
+            result = if self.counter > self.current_fun.end_pos {
+                println!("Implicit return at end of {}", self.fun_name());
                 self.ret()
             } else {
                 println!("{}\tPC {:?} {:?}\tStack: {:?}",
-                         self.current_fun.name,
+                         self.fun_name(),
                          self.counter,
-                         self.current_fun.code[self.counter],
+                         self.program[self.counter],
                          self.data_stack);
 
                 self.step()
@@ -114,66 +224,69 @@ impl<'a> Context<'a> {
 
         println!("-");
         println!("{}\tPC {:?} Stack {:?} Exit: {:?}",
-                 self.current_fun.name,
+                 self.fun_name(),
                  self.counter,
                  self.data_stack, error);
 
         (self.data_stack.clone(), error)
     }
 
-    fn step(&mut self) -> Result<()> {
-        let instr = self.current_fun.code[self.counter];
+    fn step(&mut self) -> VmResult<()> {
+        let instr = self.program[self.counter];
         self.counter += 1;
 
         match instr {
             Push(val) => Ok(self.data_stack.push(val)),
             Pop => self.pop().map(|_| ()),
 
-            Swap(pos) => self.swap(pos),
+            Swap(stack_pos) => self.swap(stack_pos),
 
             BinOp(op) => self.binop(op),
             Branch(cond, offset) => self.branch(cond, offset),
             Jump(offset) => self.jump(offset),
 
-            Call(fun_name) => self.call(fun_name),
+            Call(function_pos) => self.call(function_pos),
             Return => self.ret(),
         }
     }
 
+    fn get_fun(&self, function_pos: ProgramPos) -> VmResult<(Function, usize)> {
+        let (f, p, _) = try!(self.functions
+                          .search_start_pos(function_pos)
+                          .ok_or(InvalidFunctionPosition));
 
-    fn get_function(&mut self, fun_name: &str) -> Result<Function<'a>> {
-        self.program.get(fun_name).map(|f| *f).ok_or(FunctionNotFound)
+        Ok((f, p))
     }
 
-    fn call(&mut self, fun_name: &'a str) -> Result<()> {
-        let fun = try!(self.get_function(fun_name));
-
-        self.call_stack.push(ReturnValue {
-            function: self.current_fun.name,
-            counter: self.counter,
+    fn call(&mut self, function_pos: ProgramPos) -> VmResult<()> {
+        // the stack was previously advanced
+        self.call_stack.push(CallFrame {
+            return_pos: self.counter - 1,
+            function_start: self.current_fun.start_pos
         });
-        self.counter = 0;
+        self.counter = function_pos;
+        let (fun, pos) = try!(self.get_fun(function_pos));
         self.current_fun = fun;
+        self.current_fun_pos = pos;
 
         Ok(())
     }
 
-    fn ret(&mut self) -> Result<()> {
-        let val = try!(self.call_stack.pop().ok_or(Halted));
+    fn ret(&mut self) -> VmResult<()> {
+        let frame = try!(self.call_stack.pop().ok_or(Halted));
+        self.counter = frame.return_pos + 1;
+        let (fun, pos) = try!(self.get_fun(frame.function_start));
+        self.current_fun = fun;
+        self.current_fun_pos = pos;
 
-        self.current_fun = try!(self.get_function(val.function));
-
-        self.counter = val.counter;
         Ok(())
     }
 
-
-
-    fn pop(&mut self) -> Result<Literal> {
+    fn pop(&mut self) -> VmResult<Literal> {
         self.data_stack.pop().ok_or(StackUnderflow)
     }
 
-    fn swap(&mut self, pos: StackPos) -> Result<()> {
+    fn swap(&mut self, pos: StackPos) -> VmResult<()> {
         let last = self.data_stack.len() - 1;
 
         if pos > last {
@@ -184,7 +297,7 @@ impl<'a> Context<'a> {
         Ok(())
     }
 
-    fn binop(&mut self, op: Operation) -> Result<()> {
+    fn binop(&mut self, op: Operation) -> VmResult<()> {
         let a = try!(self.pop());
         let b = try!(self.pop());
 
@@ -197,7 +310,7 @@ impl<'a> Context<'a> {
         Ok(())
     }
 
-    fn branch(&mut self, cond: BranchCondition, offset: JumpOffset) -> Result<()> {
+    fn branch(&mut self, cond: BranchCondition, offset: JumpOffset) -> VmResult<()> {
         let a = try!(self.pop());
         let b = try!(self.pop());
 
@@ -208,46 +321,50 @@ impl<'a> Context<'a> {
         Ok(())
     }
 
-    fn jump(&mut self, offset: JumpOffset) -> Result<()> {
-        let address = (self.counter as JumpOffset) + offset;
-        let last = (self.current_fun.code.len() - 1) as JumpOffset;
+    fn jump(&mut self, offset: JumpOffset) -> VmResult<()> {
+        let address = ((self.counter as JumpOffset) + offset - 1) as StackPos;
 
-        if address < 0 || address > last {
-            return Err(InvalidJumpOffset);
+        if address < self.current_fun.start_pos {
+            return Err(JumpOffsetTooSmall);
+        }
+        if address > self.current_fun.end_pos {
+            return Err(JumpOffsetTooLarge);
         }
 
-        self.counter = address as StackPos;
+        self.counter = address;
         Ok(())
     }
 }
 
-// this should be in the standard library ...
-macro_rules! hashmap {
-    ($( $key: expr => $val: expr ),*) => {{
-         let mut map = HashMap::new();
-         $( map.insert($key, $val); )*
-         map
-    }}
+#[test]
+fn empty() {
+    let mut c = Context::new();
+    assert_eq!(c.run(), (vec![], ThereIsNoCode));
 }
 
+#[test]
+fn add() {
+    let mut c = Context::new();
+    c.link(vec![("main", vec![Push(15), Push(6), BinOp(Add)])]).unwrap();
+    assert_eq!(c.run(), (vec![21], Halted));
+}
 
+#[test]
+fn noop() {
+    // Jump(1) executes the next instruction (i.e. it's a NOOP).
+    // Just vec![Jump(1)] doesn't actually work though.
+
+    let mut c = Context::new();
+    c.link(vec![("main", vec![Jump(1), Return])]).unwrap();
+    assert_eq!(c.run(), (vec![], Halted));
+}
 
 #[test]
 fn double() {
-    let main = vec![Push(1), Call("double"), Push(5), BinOp(Add)];
-    let double = vec![Push(2), BinOp(Mul)];
+    let main : Vec<Opcode<&str>> = vec![Push(27), Call("double"), Push(5), BinOp(Add)];
+    let double : Vec<Opcode<&str>> = vec![Push(2), BinOp(Mul)];  
 
-    let program = hashmap! {
-        "main" => Function {
-            name: "main",
-            code: &main
-        },
-        "double" => Function {
-            name: "double",
-            code: &double
-        }
-    };
-
-    let mut context = Context::new(program).unwrap();
-    assert_eq!(context.run(), (vec![7], Halted));
+    let mut c = Context::new();
+    c.link(vec![("main", main), ("double", double)]).unwrap();
+    assert_eq!(c.run(), (vec![59], Halted));
 }
